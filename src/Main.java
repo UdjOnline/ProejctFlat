08f74768a18c71863e968a53483a4501c8e69144
Sergej Schmidt <sergej-schmidt@online.de>
2024-03-09 10:41:42 +0100

    added new methods in manager class and loop and Switch case in Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,95 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        House house1 = new House("inSpanndau", 25);
+        //House house1 = new House("inSpanndau", 25);
 
-        List<Flat> flats = new LinkedList<>();
+
+        //ввести массив квартир:
+        //перевести массив в LinkedList через for цикл.
+        //List<Flat> flats = new LinkedList<>();
+
+        System.out.println("добро пожаловать в Манэджер квартир");
+
+        boolean loopIsTrue = true;
+
+        Scanner scanner = new Scanner(System.in);
+        Manager manager = new Manager();
+
+        do {
+            manager.getIntro();
+
+            String lineIn = scanner.nextLine();
+
+            switch (lineIn) {
+                case "exit":
+                    System.out.println("до свиданья");
+                    loopIsTrue = false;
+                    break;
+
+                case "help":
+                    manager.getHelp();
+                    break;
+
+                case "info":
+                    manager.getInfo();
+                    break;
+
+                case "show":
+                    manager.getshow();
+                    break;
+
+                case "add":
+                    manager.add();
+                    break;
+
+                case "update_by_id":
+                    manager.update_by_id();
+                    break;
+
+                case "remove_by_id":
+                    manager.remove_by_id();
+                    break;
+
+                case "clear":
+                    manager.clear();
+                    break;
+
+                case "remove_head":
+                    manager.remove_head();
+                    break;
+
+                case "history":
+                    manager.history();
+                    break;
+
+                case "filter_less_than_balcony":
+                    manager.filter_less_than_balcony();
+                    break;
+
+                default:
+                    System.err.println("не верно задонноя строка");
+
+            }
+        } while (loopIsTrue);
+
 
 
     }
 }
+//
+//help : вывести справку по доступным командам
+//info : вывести в стандартный поток вывода информацию о коллекции (тип, дата инициализации, количество элементов и т.д.)
+//show : вывести в стандартный поток вывода все элементы коллекции в строковом представлении
+//add : добавить новый элемент в коллекцию (спросить про значение каждого поля)
+//update_by_id {id}: обновить значение элемента коллекции, id которого равен заданному (спросить что вы хотите изменить, вы должны указать в вводе id той или иной квартиры, и тогда программа должна спросить какое поле вы хотите обновить и т.д.)
+//remove_by_id {id} : удалить элемент из коллекции по его id
+//clear : очистить коллекцию
+//exit : завершить программу (без сохранения в файл)
+//remove_head : вывести первый элемент коллекции и удалить его
+//history : вывести последние 15 команд (без их аргументов)
+//filter_less_than_balcony {balcony} : вывести элементы, значение поля balcony которых меньше заданного
+//print_ascending : вывести элементы коллекции в порядке возрастания
--- src/Manager.java
+++ src/Manager.java
@@ -26,4 +26,55 @@ public class Manager {
             return 0;
         }
     }
+
+    public void getIntro() {
+        System.out.println("Введите, что надо сделать");
+    }
+
+    public void getHelp() {
+        System.out.println("Здесь будет выданна помощь!");
+    }
+
+    public void getInfo() {
+        System.out.println("Здесь будет выданна Информация о коллекции!");
+    }
+
+    public void getshow() {
+        System.out.println("Здесь будет выданны все элементы коллекции!");
+    }
+
+
+    public void add() {
+        System.out.println("добавить новый элемент в коллекцию!");
+    }
+
+    public void update_by_id() {
+        System.out.println("обновить значение элемента коллекции, по id ");
+    }
+
+    public void remove_by_id() {
+        System.out.println("удалить элемент из коллекции по его id! ");
+    }
+
+    public void clear() {
+        System.out.println("очистить коллекцию! ");
+    }
+
+    public void remove_head() {
+        System.out.println("вывести первый элемент коллекции и удалить его! ");
+    }
+
+    public void history() {
+        System.out.println("вывести последние 15 команд! ");
+    }
+
+    public void filter_less_than_balcony() {
+        System.out.println("вывести элементы, значение поля balcony! ");
+    }
+
+    public void print_ascending() {
+        System.out.println("вывести элементы коллекции в порядке возрастания! ");
+    }
+
+
 }
